eb5da4cb28b86db1d7514f7e742c8ef49d1dc4b3
OmkarGeedh <40747153+OmkarGeedh@users.noreply.github.com>
2024-11-28 17:30:06 +0530

    Initial Commit

---
+++ Swipe Billing/Swipe_BillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_BillingApp.swift
+//  Swipe Billing
+//
+//  Created by Omkar Geedh on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_BillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swipe BillingTests/Swipe_BillingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_BillingTests.swift
+//  Swipe BillingTests
+//
+//  Created by Omkar Geedh on 28/11/24.
+//
+
+import Testing
+@testable import Swipe_Billing
+
+struct Swipe_BillingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
